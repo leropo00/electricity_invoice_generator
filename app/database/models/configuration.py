384e6fdc55054342d33889b989ae69b87c6afd67
leropo00 <leropo00@gmail.com>
2025-09-08 23:12:39 +0200

    added configuration for hourly block levels

--- app/database/models/configuration.py
+++ app/database/models/configuration.py
@@ -1,11 +1,14 @@
-from sqlalchemy import  Boolean, Computed, CheckConstraint, Integer, String
+from enum import Enum
+from typing import List
+
+from sqlalchemy import  Boolean, Computed, CheckConstraint, Enum as SqlEnum, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..base import Base
 from ..mixins import TimestampMixin
 
 
-class ElectricitySeasons(Base, TimestampMixin):
+class ElectricitySeason(Base, TimestampMixin):
     __tablename__ = "config_electricity_seasons"
     __table_args__ = (
         CheckConstraint("start_month >= 1 AND start_month <= 12 AND end_month >= 1 AND end_month <= 12", name="electricity_seasons_month_values"),
@@ -15,7 +18,6 @@ class ElectricitySeasons(Base, TimestampMixin):
 
     season_key: Mapped[str] = mapped_column(String)
     season_name: Mapped[str] = mapped_column(String)
-    
     start_month: Mapped[int] = mapped_column(Integer)
     end_month: Mapped[int] = mapped_column(Integer)
 
@@ -23,3 +25,32 @@ class ElectricitySeasons(Base, TimestampMixin):
         Boolean,
         Computed("start_month > end_month", persisted=True)
     )
+    hours: Mapped[List["HourlyBlockLevel"]] = relationship(
+        back_populates="electricity_season",
+        cascade="all, delete",
+    )
+
+
+class SeasonDayType(Enum):
+    WORKDAY = "workday"
+    OFFDAY  = "offday"
+
+
+class HourlyBlockLevel(Base, TimestampMixin):
+    __tablename__ = "config_hourly_block_levels"
+    __table_args__ = (
+        CheckConstraint("level >= 1 AND level <= 5", name="hourly_block_levels_valid"),
+        CheckConstraint("hour >= 0 AND hour <= 23",  name="hourly_block_levels_hours_valid"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    level: Mapped[int] = mapped_column(Integer)
+    hour: Mapped[int] = mapped_column(Integer)
+    day_type: Mapped[SeasonDayType] = mapped_column(
+        SqlEnum(SeasonDayType, name="hourly_block_levels_day_type", native_enum=True), nullable=False
+    )
+
+    electricity_season_id: Mapped[int] = mapped_column(ForeignKey("config_electricity_seasons.id"))
+    electricity_season: Mapped["ElectricitySeason"] = relationship("ElectricitySeason", back_populates="hours")
+
