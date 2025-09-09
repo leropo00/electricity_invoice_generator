9dd813314588e7e518817f2ec7c8e84d37e395f6
leropo00 <leropo00@gmail.com>
2025-09-09 17:09:51 +0200

    added endpoint into the project

---
+++ app/endpoints/providers.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.database.session import get_db
+from app.database.models.customer import ElectricityProvider
+
+
+router = APIRouter(
+    prefix="/providers",
+)
+
+@router.get("/")
+def all_providers(session: Session = Depends(get_db)):
+    return session.execute(select(ElectricityProvider)).scalars().all()
