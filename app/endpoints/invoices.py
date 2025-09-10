979c2422c5d3ba7e69c3706d64db6237e97d1a26
leropo00 <leropo00@gmail.com>
2025-09-10 19:30:35 +0200

    refactored some logic from invoice creation to utils file

--- app/endpoints/invoices.py
+++ app/endpoints/invoices.py
@@ -6,7 +6,7 @@ from dateutil.relativedelta import relativedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import StreamingResponse
 from jinja2 import Environment, FileSystemLoader
-from sqlalchemy import bindparam, extract, text
+from sqlalchemy import extract, select
 from sqlalchemy.orm import Session, joinedload, selectinload
 from weasyprint import HTML
 
@@ -16,11 +16,18 @@ from app.database.models.invoice import ElectricityInvoice, ElectricityInvoiceIt
 from app.database.models.measurement import ElectricityUsage
 from app.database.session import get_db
 from app.schema.invoice import CreateInvoice
+from app.utils.invoice import calculate_measurements_total_usage, calculate_measurements_time_block_usage
+from app.utils.serialization import orm_object_to_dict_exclude_default
 
 router = APIRouter(
     prefix="/invoices",
     tags=["Electricity Invoices"],
 )
+
+
+@router.get("/")
+def all_invoices(session: Session = Depends(get_db)):
+    return session.execute(select(ElectricityInvoice)).scalars().all()
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 def create_invoice_record(
@@ -50,8 +57,8 @@ def create_invoice_record(
             status_code=status.HTTP_400_BAD_REQUEST, detail="No invoice records found for the selected time range"
         )
 
-    total_price, total_consumption = calculate_total(session, data.year, data.month, customer.id)
-    timeblock_usage = calculate_time_block_ranges(session, data.year, data.month, customer.id)
+    total_price, total_consumption = calculate_measurements_total_usage(session, data.year, data.month, customer.id)
+    timeblock_usage = calculate_measurements_time_block_usage(session, data.year, data.month, customer.id)
 
     issued_date = date.today()
     due_date = issued_date + relativedelta(days=data.days_payment_due)
@@ -145,137 +152,3 @@ def create_invoice_pdf_document(
         media_type="application/pdf",
         headers={"Content-Disposition": "attachment; filename="+filename}
     )
-
-
-
-def calculate_total(session: Session, year: int, month: int, customer_id: int):
-    start_date = date(year, month, 1)
-    end_date = start_date + relativedelta(months=1)
-
-    # Raw SQL query with named parameters
-    query = text("""
-        SELECT 
-            COALESCE(SUM(eem.consumption_kwh * eem.price_per_kwh), 0) AS total_price,
-            COALESCE(SUM(eem.consumption_kwh), 0) AS total_consumption
-        FROM measurements_electricity_usage eem
-        WHERE eem.customer_id = :customer_id
-        AND eem.measured_at >= :start_date
-        AND eem.measured_at < :end_date
-    """)
-
-    # Execute with parameters
-    result = session.execute(query, {
-        "customer_id": customer_id,
-        "start_date": start_date,
-        "end_date": end_date
-    }).fetchone()
-
-    # Access results
-    total_price = result.total_price
-    total_consumption = result.total_consumption
-
-    return total_price, total_consumption
-
-
-    
-def calculate_time_block_ranges(session: Session, year: int, month: int, customer_id: int):     
-    workday_blocks_with_hours = defaultdict(list)
-    offday_blocks_with_hours = defaultdict(list)
-
-    query = text("""
-        SELECT l.level, l.hour, l.day_type
-        FROM config_electricity_seasons s
-        JOIN config_hourly_block_levels l on s.id = l.electricity_season_id
-        WHERE  (s.crosses_calendar_year = FALSE AND s.start_month <= :month AND  s.end_month  >= :month )
-        OR (s.crosses_calendar_year = TRUE AND ( s.start_month <= :month AND  s.end_month  >= :month ) )
-        ORDER BY l.day_type, l.level, l.hour
-    """)
-
-    result = session.execute(query, {
-        "month": month,
-    }).all()
-    
-    for row in result:
-        level, hour, day_type = row
-        if day_type == SeasonDayType.WORKDAY.name:
-            workday_blocks_with_hours[level].append(hour)
-        elif day_type == SeasonDayType.OFFDAY.name:
-            offday_blocks_with_hours[level].append(hour)
-
-    union_keys = set(workday_blocks_with_hours.keys()) | set(offday_blocks_with_hours.keys())
-    possible_time_blocks = sorted(list(union_keys))
-    
-    start_date = date(year, month, 1)
-    end_date = start_date + relativedelta(months=1)
-    
-    # this is final date of the range
-    final_date_measurements = end_date - relativedelta(days=1)
-    
-    timeblock_usage = []
-        
-    for time_block in possible_time_blocks:           
-        price_sum, consumption_sum = 0.0, 0.0
-          
-        if time_block in workday_blocks_with_hours:
-            price, consumption = calculate_time_block(session, start_date, end_date, customer_id, workday_blocks_with_hours[time_block], SeasonDayType.WORKDAY)
-            price_sum += price
-            consumption_sum += consumption
-
-        if time_block in offday_blocks_with_hours:
-            price, consumption = calculate_time_block(session, start_date, end_date, customer_id, offday_blocks_with_hours[time_block], SeasonDayType.OFFDAY)
-            price_sum += price
-            consumption_sum += consumption
-
-        # it could be possible that price is 0 fro time block, but consumtion should still be present
-        if consumption_sum > 0:
-            timeblock_usage.append({'time_block': time_block, 'consumption': consumption_sum, 'price': price_sum, 'start_date': start_date, 'end_date': final_date_measurements})
-        
-    return timeblock_usage
-
-def calculate_time_block(session: Session, start_date: date, end_date: date, customer_id: int, hours:list, day_type: SeasonDayType):
-
-    query_string = '''
-    SELECT COALESCE(SUM(eem.consumption_kwh * eem.price_per_kwh), 0) as total_price,
-        COALESCE(SUM(eem.consumption_kwh), 0) as total_consumption
-        FROM measurements_electricity_usage eem 
-        WHERE  eem.customer_id = :customer_id
-        AND  eem.measured_at >= DATE :start_date
-        AND eem.measured_at < DATE :end_date
-        AND EXTRACT(HOUR FROM eem.measured_at) IN :hours
-    '''    
-    
-    # national holidays are currently ignored in the calculation for offdays
-    if day_type == SeasonDayType.OFFDAY:
-        query_string += ' AND EXTRACT(DOW FROM eem.measured_at) IN (0, 6) '
-    
-    elif day_type == SeasonDayType.WORKDAY:
-        query_string += ' AND EXTRACT(DOW FROM eem.measured_at) BETWEEN 1 AND 5'
-
-    query = text(query_string).bindparams(bindparam("hours", expanding=True))
-
-    result = session.execute(query, {
-        "customer_id": customer_id,
-        "start_date": start_date,
-        "end_date": end_date,
-        "hours": hours,
-    }).fetchone()
-
-    total_price = result.total_price
-    total_consumption = result.total_consumption
-    return total_price, total_consumption
-
-
-def orm_object_to_dict_exclude_default(obj, exclude_fields=None):
-    exclude_default = ['id', 'created_at', 'updated_at']
-    if exclude_fields:
-        exclude_default.extend(exclude_fields)
-    return orm_object_to_dict(obj, set(exclude_default))
-
-def orm_object_to_dict(obj, exclude_fields=None):
-    exclude_fields = exclude_fields or set()
-    #Relationships aren’t part of __table__.columns, so they’re already excluded by default. 
-    return {
-        column.name: getattr(obj, column.name)
-        for column in obj.__table__.columns
-        if column.name not in exclude_fields
-    }
---
+++ app/utils/invoice.py
@@ -0,0 +1,127 @@
+from collections import defaultdict
+from datetime import date
+import io
+
+from dateutil.relativedelta import relativedelta
+from sqlalchemy import bindparam, text
+from sqlalchemy.orm import Session
+
+from app.database.models.configuration import SeasonDayType
+
+__all__ = ["calculate_measurements_total_usage", "calculate_measurements_time_block_usage"]
+
+def calculate_measurements_total_usage(session: Session, year: int, month: int, customer_id: int):
+    start_date = date(year, month, 1)
+    end_date = start_date + relativedelta(months=1)
+
+    # Raw SQL query with named parameters
+    query = text("""
+        SELECT 
+            COALESCE(SUM(eem.consumption_kwh * eem.price_per_kwh), 0) AS total_price,
+            COALESCE(SUM(eem.consumption_kwh), 0) AS total_consumption
+        FROM measurements_electricity_usage eem
+        WHERE eem.customer_id = :customer_id
+        AND eem.measured_at >= :start_date
+        AND eem.measured_at < :end_date
+    """)
+
+    # Execute with parameters
+    result = session.execute(query, {
+        "customer_id": customer_id,
+        "start_date": start_date,
+        "end_date": end_date
+    }).fetchone()
+
+    # Access results
+    total_price = result.total_price
+    total_consumption = result.total_consumption
+
+    return total_price, total_consumption
+
+
+    
+def calculate_measurements_time_block_usage(session: Session, year: int, month: int, customer_id: int):     
+    workday_blocks_with_hours = defaultdict(list)
+    offday_blocks_with_hours = defaultdict(list)
+
+    query = text("""
+        SELECT l.level, l.hour, l.day_type
+        FROM config_electricity_seasons s
+        JOIN config_hourly_block_levels l on s.id = l.electricity_season_id
+        WHERE  (s.crosses_calendar_year = FALSE AND s.start_month <= :month AND  s.end_month  >= :month )
+        OR (s.crosses_calendar_year = TRUE AND ( s.start_month <= :month AND  s.end_month  >= :month ) )
+        ORDER BY l.day_type, l.level, l.hour
+    """)
+
+    result = session.execute(query, {
+        "month": month,
+    }).all()
+    
+    for row in result:
+        level, hour, day_type = row
+        if day_type == SeasonDayType.WORKDAY.name:
+            workday_blocks_with_hours[level].append(hour)
+        elif day_type == SeasonDayType.OFFDAY.name:
+            offday_blocks_with_hours[level].append(hour)
+
+    union_keys = set(workday_blocks_with_hours.keys()) | set(offday_blocks_with_hours.keys())
+    possible_time_blocks = sorted(list(union_keys))
+    
+    start_date = date(year, month, 1)
+    end_date = start_date + relativedelta(months=1)
+    
+    # this is final date of the range
+    final_date_measurements = end_date - relativedelta(days=1)
+    
+    timeblock_usage = []
+        
+    for time_block in possible_time_blocks:           
+        price_sum, consumption_sum = 0.0, 0.0
+          
+        if time_block in workday_blocks_with_hours:
+            price, consumption = _calculate_time_block(session, start_date, end_date, customer_id, workday_blocks_with_hours[time_block], SeasonDayType.WORKDAY)
+            price_sum += price
+            consumption_sum += consumption
+
+        if time_block in offday_blocks_with_hours:
+            price, consumption = _calculate_time_block(session, start_date, end_date, customer_id, offday_blocks_with_hours[time_block], SeasonDayType.OFFDAY)
+            price_sum += price
+            consumption_sum += consumption
+
+        # it could be possible that price is 0 fro time block, but consumtion should still be present
+        if consumption_sum > 0:
+            timeblock_usage.append({'time_block': time_block, 'consumption': consumption_sum, 'price': price_sum, 'start_date': start_date, 'end_date': final_date_measurements})
+        
+    return timeblock_usage
+
+def _calculate_time_block(session: Session, start_date: date, end_date: date, customer_id: int, hours:list, day_type: SeasonDayType):
+
+    query_string = '''
+    SELECT COALESCE(SUM(eem.consumption_kwh * eem.price_per_kwh), 0) as total_price,
+        COALESCE(SUM(eem.consumption_kwh), 0) as total_consumption
+        FROM measurements_electricity_usage eem 
+        WHERE  eem.customer_id = :customer_id
+        AND  eem.measured_at >= DATE :start_date
+        AND eem.measured_at < DATE :end_date
+        AND EXTRACT(HOUR FROM eem.measured_at) IN :hours
+    '''    
+    
+    # national holidays are currently ignored in the calculation for offdays
+    if day_type == SeasonDayType.OFFDAY:
+        query_string += ' AND EXTRACT(DOW FROM eem.measured_at) IN (0, 6) '
+    
+    elif day_type == SeasonDayType.WORKDAY:
+        query_string += ' AND EXTRACT(DOW FROM eem.measured_at) BETWEEN 1 AND 5'
+
+    query = text(query_string).bindparams(bindparam("hours", expanding=True))
+
+    result = session.execute(query, {
+        "customer_id": customer_id,
+        "start_date": start_date,
+        "end_date": end_date,
+        "hours": hours,
+    }).fetchone()
+
+    total_price = result.total_price
+    total_consumption = result.total_consumption
+    return total_price, total_consumption
