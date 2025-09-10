fc5481770204f7d85c6ae5686ed2fe5ec8d8af9d
leropo00 <leropo00@gmail.com>
2025-09-10 11:44:40 +0200

    added custom types for year and month

--- app/schema/custom_type.py
+++ app/schema/custom_type.py
@@ -1,10 +1,27 @@
-from pydantic import AfterValidator
 from typing import Annotated
+
+from pydantic import AfterValidator
 
 def validate_four_digit_zip(value: int) -> int:
     if not (1000 <= value <= 9999):
         raise ValueError("zip_code must be exactly 4 digits")
     return value
 
-
 ZipCode = Annotated[int, AfterValidator(validate_four_digit_zip)]
+
+def validate_month(value: int) -> int:
+    if not (1 <= value <= 12):
+        raise ValueError("month must be inside 1 -12 range")
+    return value
+
+
+MonthType = Annotated[int, AfterValidator(validate_month)]
+
+
+def validate_year(value: int) -> int:
+    if not (value >= 1990):
+        raise ValueError("aloowed year are from 1990 onwards")
+    return value
+
+
+YearType = Annotated[int, AfterValidator(validate_year)]
